1c647e7c2c623e1efeb7df96ca1feeed127aebb3
Tomoki P <tmptsh@gmail.com>
2021-09-08 20:01:50 -0500

    Add activities 316 and 317

--- ClassActivities/3.1.7-ArrowSyntax.js
+++ ClassActivities/3.1.7-ArrowSyntax.js
@@ -1,2 +1,47 @@
 //https://github.com/savvy-coders/sc-curriculum/blob/master/Section03/Activities/3.1-Activities/3.1.7-ArrowSyntax.md
 
+// refactor the functions below into arrow syntax
+/* function myNumberFunction() {
+  return ((2 ** 2 + 3) % 4) * 14;
+}
+*/
+const myNumberFunction = (n) => ((2 ** 2 + 3) % 4) * 14;
+
+
+/*function greet(name) {
+  return `Hello, ${name}`;
+}*/
+const greet = (name) => `Hello, ${name}`;
+
+
+/*const timeOfDayGreet = function(name, timeOfDay) {
+  return `Hello, ${name}, good ${timeOfDay}`;
+};*/
+const timeOfDayGreet = (name, timeOfDay) => `Hello, ${name}, good ${timeOfDay}`;
+
+
+/*function tripleAndHalf(num) {
+  let triple = num * 3;
+  return triple / 2;
+}*/
+const tripleAndHalf = (num) => {
+  let triple = num * 3;
+  return triple / 2;
+}
+
+/*function sumTwoNumbers(num1, num2) {
+  let sum = num1 + num2;
+  console.log(`The sum of your numbers is ${sum}.`);
+  return sum;
+}*/
+const sumTwoNumbers = (num1, num2) => {
+  let sum = num1 + num2;
+  console.log(`The sum of your numbers is ${sum}.`);
+  return sum;
+}
+
+console.log(`myNumberFunction:: ${myNumberFunction(10)}`); //myNumberFunction
+console.log((`greet:: ${greet("Harold Donniver Tregold")}`)); //greet
+console.log((`timeOfDayGreet:: ${timeOfDayGreet("jameston","Evening")}`)); //timeOfDayGreet
+console.log((`tripleAndHalf:: ${tripleAndHalf(10)}`)); //tripleAndHalf
+console.log((`sumTwoNumbers:: ${sumTwoNumbers(10,11)}`)); //sumTwoNumbers
